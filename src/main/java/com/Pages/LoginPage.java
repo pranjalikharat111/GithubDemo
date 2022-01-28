d9c4df50befdf90e56ff20ca56e761fd0852dea1
pranjalikharat111 <pranjalikharat111@gmail.com>
2022-01-28 15:19:49 +0530

    Elicpse changes

--- src/main/java/com/Pages/LoginPage.java
+++ src/main/java/com/Pages/LoginPage.java
@@ -4,5 +4,6 @@ public class LoginPage {
 public void  loginpage() {
 	System.out.println("LoginPage method");
 	System.out.println("Login page");
+	System.out.println("The commit new changes");
 }
 }
